f3f51a67889637e67e882135c98d3b79905541fb
THILAKGATS <thilakrock1799@gmail.com>
2023-03-29 15:28:28 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+// Navbar Actions
+const offcanvas = document.querySelectorAll(".offcanvas");
+const menu = document.getElementsByClassName("menu");
+const close = document.getElementsByClassName("close-button");
+const nav = document.getElementsByTagName("nav");
+const navLinks = document.querySelectorAll(".nav-links");
+const form = document.getElementById("form");
+let myMail = "thilakg399@gmail.com";
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let message = document.getElementById("message");
+
+clickAction(menu[0], function () {
+  nav[0].style.display = "none";
+  offcanvas[0].style.display = "block";
+});
+
+clickAction(close[0], function () {
+  nav[0].style.display = "flex";
+  offcanvas[0].style.display = "none";
+});
+
+navLinks.forEach((item) => {
+  clickAction(item, function () {
+    nav[0].style.display = "flex";
+    offcanvas[0].style.display = "none";
+  });
+});
+
+function clickAction(element, callback) {
+  element.addEventListener("click", callback);
+}
+
+// Button's Navigation
+const button = document.querySelectorAll(".clickable");
+
+button.forEach((link) => {
+  clickAction(link, (e) => {
+    e.view.location.href = getChildLink(link);
+  });
+});
+
+function getChildLink(element) {
+  return element.children[0].href;
+}
+
+// Form submit and go to mail
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  window.open(
+    `mailto:${myMail}?subject=${email.value}&body=${name.value}:${message.value}`
+  );
+});
